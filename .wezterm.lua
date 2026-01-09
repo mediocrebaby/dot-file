33412fdd6c5443c4906de7152c541b2ecf6fcc9d
mediocrebaby <88031150+mediocrebaby@users.noreply.github.com>
2026-01-10 03:24:17 +0800

    Add ALT key bindings for tab activation
    Add key bindings for quick tab switching using ALT + number keys.

--- .wezterm.lua
+++ .wezterm.lua
@@ -196,6 +196,17 @@ config.keys = {
 	{ key = "Home", mods = "LEADER", action = wezterm.action.ScrollToTop },
 	{ key = "End", mods = "LEADER", action = wezterm.action.ScrollToBottom },
 	{ key = "v", mods = "CTRL", action = wezterm.action.PasteFrom("Clipboard") },
+	-- ALT/OPTION + 数字:快速切换到对应标签页
+	{ key = "1", mods = "ALT", action = wezterm.action.ActivateTab(0) },
+	{ key = "2", mods = "ALT", action = wezterm.action.ActivateTab(1) },
+	{ key = "3", mods = "ALT", action = wezterm.action.ActivateTab(2) },
+	{ key = "4", mods = "ALT", action = wezterm.action.ActivateTab(3) },
+	{ key = "5", mods = "ALT", action = wezterm.action.ActivateTab(4) },
+	{ key = "6", mods = "ALT", action = wezterm.action.ActivateTab(5) },
+	{ key = "7", mods = "ALT", action = wezterm.action.ActivateTab(6) },
+	{ key = "8", mods = "ALT", action = wezterm.action.ActivateTab(7) },
+	{ key = "9", mods = "ALT", action = wezterm.action.ActivateTab(8) },
+
 }
 
 ---鼠标行为---
