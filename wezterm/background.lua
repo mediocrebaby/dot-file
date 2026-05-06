8b7f24c4558e9bb557e7921ac6875838ac97f473
mediocrebaby <1009462586@qq.com>
2026-05-06 21:02:06 +0800

    style(wezterm): 文本背景设为完全不透明
    将 text_background_opacity 由 0.5 调整为 1，提升终端文字在背景图上的可读性。

--- wezterm/background.lua
+++ wezterm/background.lua
@@ -4,7 +4,7 @@ local wezterm = require 'wezterm'
 function M.apply(config)
 	---透明效果---
 	config.window_background_opacity = 0.3
-	config.text_background_opacity = 0.5
+	config.text_background_opacity = 1
 
 	---窗口图片设置---
 	config.background = {
